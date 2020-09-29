40b0259ab272c9f4436fde3c97f184de1199f399
Rsja12 <asocarras10@gmail.com>
2020-09-29 15:30:44 -0400

    ViewController adopts UIImagePickerControllerDelegate and UINavigationControllerDelegate

--- FoodTracker/ViewController.swift
+++ FoodTracker/ViewController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class ViewController: UIViewController, UITextFieldDelegate {
+class ViewController: UIViewController, UITextFieldDelegate, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
     
     //MARK: Properties
     @IBOutlet weak var nameTextField: UITextField!
