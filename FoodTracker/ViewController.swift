f7581c0fcab24032cb2cd9d03a781e3c23053496
Rsja12 <asocarras10@gmail.com>
2020-09-29 15:14:56 -0400

    add tap gesture recognizer to imageView and create selectImageFromPhotoLibrary action from it in ViewController

--- FoodTracker/ViewController.swift
+++ FoodTracker/ViewController.swift
@@ -34,6 +34,9 @@ class ViewController: UIViewController, UITextFieldDelegate {
     }
 
     //MARK: Actions
+    @IBAction func selectImageFromPhotoLibrary(_ sender: UITapGestureRecognizer) {
+    }
+    
     @IBAction func setDefaultLabelText(_ sender: UIButton) {
         mealNameLabel.text = "Default Text"
     }
