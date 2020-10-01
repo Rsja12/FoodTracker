376de79064b7166bb469429baa20f586547fcfc5
Rsja12 <asocarras10@gmail.com>
2020-10-01 15:22:15 -0400

    update numberOfSections(in:) method and tableView(numberOfRowsInSection) method

--- FoodTracker/MealTableViewController.swift
+++ FoodTracker/MealTableViewController.swift
@@ -21,16 +21,14 @@ class MealTableViewController: UITableViewController {
     // MARK: - Table view data source
 
     override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
+        return 1
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 0
+        return meals.count
     }
 
-    /*
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
 
@@ -38,7 +36,7 @@ class MealTableViewController: UITableViewController {
 
         return cell
     }
-    */
+    
 
     /*
     // Override to support conditional editing of the table view.
